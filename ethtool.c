pmachata/ethtool

commit 6e3ac505bcc778b04d43dd1858af8287e8a08ef7

Add runtime support for disabling netlink
Provide --disable-netlink option for disabling netlink during runtime,
without the need to recompile the binary.

Signed-off-by: Krzysztof Piotr Oledzki <ole@ans.pl>

ethtool.c
@@ -6537,6 +6537,12 @@ int main(int argc, char **argp)
 			argc -= 2;
 			continue;
 		}
+		if (*argp && !strcmp(*argp, "--disable-netlink")) {
+			ctx.nl_disable = true;
+			argp += 1;
+			argc -= 1;
+			continue;
+		}
 		if (*argp && !strcmp(*argp, "--json")) {
 			ctx.json = true;
 			argp += 1;

ethtool.8.in
@@ -137,6 +137,9 @@ ethtool \- query or control network driver and hardware settings
 .BN --debug
 .I args
 .HP
+.B ethtool [--disable-netlink]
+.I args
+.HP
 .B ethtool [--json]
 .I args
 .HP
@@ -580,6 +583,9 @@ lB	l.
 0x08  Hex dump of received netlink messages
 0x10  Structure of netlink messages
 .TE
+.TP
+.BI \-\-disable-netlink
+Do not use netlink and fall back to the ioctl interface if possible.
 .TP
 .BI \-\-json
 Output results in JavaScript Object Notation (JSON). Only a subset of

internal.h
@@ -221,6 +221,7 @@ struct cmd_context {
 	char **argp;		/* arguments to the sub-command */
 	unsigned long debug;	/* debugging mask */
 	bool json;		/* Output JSON, if supported */
+	bool nl_disable;	/* Disable netlink even if available */
 	bool show_stats;	/* include command-specific stats */
 #ifdef ETHTOOL_ENABLE_NETLINK
 	struct nl_context *nlctx;	/* netlink context (opaque) */

netlink/netlink.c
@@ -470,6 +470,11 @@ void netlink_run_handler(struct cmd_context *ctx, nl_chk_t nlchk,
 	const char *reason;
 	int ret;
 
+	if (ctx->nl_disable) {
+		reason = "netlink disabled";
+		goto no_support;
+	}
+
 	if (nlchk && !nlchk(ctx)) {
 		reason = "ioctl-only request";
 		goto no_support;

msft-mirror-aosp/platform.external.ethtool

commit 730953623c62920a33c55f9ecc012df238ce7e4f

ethtool: Add --disable-netlink to help output
The --disable-netlink flag is documented in the man page but missing
from the usage output shown by ethtool -h.

Add it to the FLAGS section.

Fixes: 6e3ac505bcc7 ("Add runtime support for disabling netlink")
Reviewed-by: Nimrod Oren <noren@nvidia.com>
Signed-off-by: Gal Pressman <gal@nvidia.com>

ethtool.c
@@ -6416,6 +6416,7 @@ static int show_usage(struct cmd_context *ctx __maybe_unused)
 	fprintf(stdout, "\n");
 	fprintf(stdout, "FLAGS:\n");
 	fprintf(stdout, "	--debug MASK	turn on debugging messages\n");
+	fprintf(stdout, "	--disable-netlink	do not use netlink, fall back to ioctl interface\n");
 	fprintf(stdout, "	-j|--json	enable JSON output format (not supported by all commands)\n");
 	fprintf(stdout, "	-I|--include-statistics		request device statistics related to the command (not supported by all commands)\n");
 